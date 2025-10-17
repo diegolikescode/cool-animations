0db1af9dc1b1f34fdb1fe089c4d9404a205c40b4
diegolikescode <diegoprestes.git@hotmail.com>
2025-10-17 02:10:34 -0300

    config stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './main.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
-        {/*
-        <main className={`theme-penumbra w-full h-full`}>
-            <App />
-        </main>
-        */}
     </StrictMode>
 )
